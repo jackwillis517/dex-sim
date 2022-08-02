343fedf77cb3c502ebee3e711e3ddae36ce0666e
Jack Willis <jackwillis517@gmail.com>
2022-08-02 18:29:23 -0400

    Added deploy script to update frontend abi and contractAddresses when needed

--- deploy/99-update-front-end.js
+++ deploy/99-update-front-end.js
@@ -1,33 +1,33 @@
-// const { ethers, network } = require("hardhat")
-// const fs = require("fs")
+const { ethers, network } = require("hardhat")
+const fs = require("fs")
 
-// const FRONT_END_ADDRESSES_FILE = "../time-vault-frontend/constants/contractAddresses.json"
-// const FRONT_END_ABI_FILE = "../time-vault-frontend/constants/abi.json"
+const FRONT_END_ADDRESSES_FILE = "../dex-sim-frontend/constants/contractAddresses.json"
+const FRONT_END_ABI_FILE = "../dex-sim-frontend/constants/abi.json"
 
-// module.exports = async function () {
-//     if(process.env.UPDATE_FRONT_END){
-//         updateContractAddresses()
-//         updateAbi()
-//     }
-// }
+module.exports = async function () {
+    if(process.env.UPDATE_FRONT_END){
+        updateContractAddresses()
+        updateAbi()
+    }
+}
 
-// async function updateAbi() {
-//     const timeVault = await ethers.getContract("TimeVault")
-//     fs.writeFileSync(FRONT_END_ABI_FILE, timeVault.interface.format(ethers.utils.FormatTypes.json))
-// }
+async function updateAbi() {
+    const dexSim = await ethers.getContract("DexSim")
+    fs.writeFileSync(FRONT_END_ABI_FILE, dexSim.interface.format(ethers.utils.FormatTypes.json))
+}
 
-// async function updateContractAddresses() {
-//     const timeVault = await ethers.getContract("TimeVault")
-//     const chainId = network.config.chainId.toString()
-//     const currentAddresses = JSON.parse(fs.readFileSync(FRONT_END_ADDRESSES_FILE, "utf-8"))
-//     if(chainId in currentAddresses){
-//         if(!currentAddresses[chainId].includes(timeVault.address)){
-//             currentAddresses[chainId].push(timeVault.address)
-//         }
-//     } else {
-//         currentAddresses[chainId] = [timeVault.address]
-//     }
-//     fs.writeFileSync(FRONT_END_ADDRESSES_FILE, JSON.stringify(currentAddresses))
-// }
+async function updateContractAddresses() {
+    const dexSim = await ethers.getContract("DexSim")
+    const chainId = network.config.chainId.toString()
+    const currentAddresses = JSON.parse(fs.readFileSync(FRONT_END_ADDRESSES_FILE, "utf-8"))
+    if(chainId in currentAddresses){
+        if(!currentAddresses[chainId].includes(dexSim.address)){
+            currentAddresses[chainId].push(dexSim.address)
+        }
+    } else {
+        currentAddresses[chainId] = [dexSim.address]
+    }
+    fs.writeFileSync(FRONT_END_ADDRESSES_FILE, JSON.stringify(currentAddresses))
+}
 
-// module.exports.tags = ["all", "frontend"]
+module.exports.tags = ["all", "frontend"]
